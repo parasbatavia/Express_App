66b656aec4cf5e2e60735dc75eb3fc6467f60781
parasbatavia <paras@labhsoftware.com>
2025-05-20 21:18:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const fs = require('fs');
 const app = express();
 app.use(express.json());
 
-const port = process.env.PORT || 3000;
+//const port = process.env.PORT || 3000;
 
 // GPT setup
 const openai = new OpenAI({
